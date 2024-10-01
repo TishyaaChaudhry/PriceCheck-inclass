3e771813c8541bd80d3cac5485bcef13b42eaefb
Tishyaa Chaudhry <tishyaa.chaudhry@gmail.com>
2024-10-01 15:19:29 -0400

    inclass

--- PriceCheckFB/DataRepositories/LocationRepository.swift
+++ PriceCheckFB/DataRepositories/LocationRepository.swift
@@ -5,27 +5,35 @@
 
 
 import Combine
+import Combine
+import FirebaseFirestore
 // import Firebase modules here
-
-
 
 
 class LocationRepository: ObservableObject {
   // Set up properties here
+  private let path: String = "locations"
+  private let store = Firestore.firestore()
+
   
-  
+  @Published var locations: [Location] = []
+   private var cancellables: Set<AnyCancellable> = []
+
   
   init() {
     get()
   }
 
   func get() {
-    // Complete this function
-    
-    
-    
-    
-    
+    .addSnapshotListener { querySnapshot, error in
+           if let error = error {
+             print("Error getting books: \(error.localizedDescription)")
+             return
+           }
 
+           self.books = querySnapshot?.documents.compactMap { document in
+             try? document.data(as: Book.self)
+           } ?? []
+         }
   }
 }
